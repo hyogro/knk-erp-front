772d1014ab022fac6089dd553bb867e6e05ebb2e
HyunjuIm <dear.jjwim@gmail.com>
2021-06-08 14:19:54 +0900

    임현주_210608_14:19_로그인 페이지 UI 구성

--- routes/index.js
+++ routes/index.js
@@ -10,5 +10,8 @@ router.get('/', function (req, res, next) {
 router.get('/login', function (req, res, next) {
     res.render('login', {api: api, title: '구이앤금우통신:로그인'});
 });
+router.get('/my-page', function (req, res, next) {
+    res.render('my-page', {api: api, title: '구이앤금우통신:내 정보'});
+});
 
 module.exports = router;
