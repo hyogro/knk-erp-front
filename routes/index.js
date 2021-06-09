8d7a684688a5a3c1a99cddf658bb78c89c029c38
HyunjuIm <dear.jjwim@gmail.com>
2021-06-09 18:01:33 +0900

    임현주_210609_18:00_출퇴근 기록 시간 api

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-const api = 'https://api.leescode.com/';
-// const api = 'http://localhost:8000/';
+//const api = 'https://api.leescode.com/';
+const api = 'http://127.0.0.1:8000/';
 
 
 router.get('/', function (req, res, next) {
