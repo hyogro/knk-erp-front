beb689504f9caec372738fd1100c27e08cb4147b
HyunjuIm <dear.jjwim@gmail.com>
2021-06-25 16:06:24 +0900

    임현주_210625_16:06_참조 대상 옵션 셋팅

--- views/board/board-write.js
+++ views/board/board-write.js
@@ -1,5 +1,5 @@
 var EDITORS = [];
-window.onload = function() {
+window.onload = function () {
     // smartEditor 세팅
     nhn.husky.EZCreator.createInIFrame({
         oAppRef: EDITORS,
@@ -8,9 +8,9 @@ window.onload = function() {
         fCreator: "createSEditor2",
     });
 
-    setTimeout(function() {
+    setTimeout(function () {
         var ctntarea = document.querySelector("iframe").contentWindow.document.querySelector("iframe").contentWindow.document.querySelector(".se2_inputarea");
-        ctntarea.addEventListener("keyup", function(e) {
+        ctntarea.addEventListener("keyup", function (e) {
             var text = this.innerHTML;
             text = text.replace(/<br>/ig, "");	// br 제거
             text = text.replace(/&nbsp;/ig, "");// 공백 제거
@@ -19,13 +19,32 @@ window.onload = function() {
             var len = text.length;
             document.querySelector(".count span").innerHTML = len;
 
-            if(len > 40000) {
+            if (len > 40000) {
                 alert("최대 40000 byte까지 입력 가능합니다.");
             }
         });
     }, 1000)
 }
 
+//참조 대상 - 전체 멤버 셋팅
 $('#reference').select2({
     placeholder: '참조시킬 대상의 이름이나 아이디를 선택(입력)해주세요.'
 });
+
+request('GET', 'board/memberIdList', setSelectOptionMemberList);
+
+function setSelectOptionMemberList(res) {
+    if (res.code === null) {
+        return;
+    }
+    if (res.code === 'MIL001') {
+        let data = res.memberList;
+        for (let i = 0; i < data.length; i++) {
+            let html = '<option value=\'' + data[i].memberId + '\'>' +
+                data[i].memberName + '(' + data[i].memberId + ')</option>'
+            $("#reference").append(html);
+        }
+    } else if (res.code === 'MIL002') {
+        console.log("멤버 id, 이름 목록 불러오기 실패");
+    }
+}
