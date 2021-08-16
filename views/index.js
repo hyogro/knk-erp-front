36c128e057f2e3d485641d24bdb4665746165180
shlee <dltkdgns3435@naver.com>
2021-08-17 08:50:55 +0900

    shlee_20210817_0850 update 출퇴근기록 정정

--- views/index.js
+++ views/index.js
@@ -199,20 +199,21 @@ function setCalendar(data) {
 
 //출,퇴근 기록
 function checkWork(type) {
-    let allowIP = ['61.42.17.186'/*회사아이피*/, '59.1.168.71'/*재택근무자*/]; // 허용할 IP
+    /*
+    let allowIP = ['61.42.17.186', '59.1.168.71']; // 허용할 IP
     let remoteIp = ip(); // 사용자 IP
-
-    if (0 <= allowIP.indexOf(remoteIp)) {
-        if (type === 'onWork') {
-            request('POST', 'attendance/onWork', onWork);
-        } else if (type === 'offWork') {
-            request('POST', 'attendance/offWork', offWork);
-        } else {
-            alert('올바른 요청이 아닙니다.');
-        }
+    */
+    //if (0 <= allowIP.indexOf(remoteIp)) {
+    if (type === 'onWork') {
+        request('POST', 'attendance/onWork', onWork);
+    } else if (type === 'offWork') {
+        request('POST', 'attendance/offWork', offWork);
     } else {
-        alert('요청하신 주소: ' + remoteIp + ' 에서는 출/퇴근 기록을 할 수 없습니다.');
+        alert('올바른 요청이 아닙니다.');
     }
+    // } else {
+    //     alert('요청하신 주소: ' + remoteIp + ' 에서는 출/퇴근 기록을 할 수 없습니다.');
+    // }
 }
 
 //출근 기록 찍기
