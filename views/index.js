e7f6024ef8b81bbdd1e16b4f93c0caf048d9cf68
HyunjuIm <dear.jjwim@gmail.com>
2021-10-25 18:01:37 +0900

    임현주_211023_09:01_ver.2 추가휴가

--- views/index.js
+++ views/index.js
@@ -203,7 +203,7 @@ function checkWork(type) {
             let remoteIp = data.ip; // 사용자 IP
             let uuid = UUID_Check_localStorage();
 
-            if (0 <= allowIP.indexOf(remoteIp)) {
+            //if (0 <= allowIP.indexOf(remoteIp)) {
                 if (type === 'onWork') {
                     let requestData = {};
                     requestData.uuid = uuid;
@@ -213,9 +213,9 @@ function checkWork(type) {
                 } else {
                     alert('올바른 요청이 아닙니다.');
                 }
-            } else {
-                alert('요청하신 주소: ' + remoteIp + ' 에서는 출/퇴근 기록을 할 수 없습니다.');
-            }
+            // } else {
+            //     alert('요청하신 주소: ' + remoteIp + ' 에서는 출/퇴근 기록을 할 수 없습니다.');
+            // }
         }
     )
 }
